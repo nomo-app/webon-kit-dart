853efb435f58b8124d2740805d7539dd3b46867b
dev2 <dev2@nomo.app>
2024-07-11 19:12:28 +0200

    Implement NomoSignTransaction

--- lib/src/bridges/wallet_bridge.dart
+++ lib/src/bridges/wallet_bridge.dart
@@ -5,11 +5,15 @@ import 'dart:js_interop';
 external JSPromise<JSString> nomoGetEvmAddress();
 
 @JS()
-external JSPromise<JSString> nomoSignEvmTransaction(SignEvmArgs args);
+external JSPromise<SignEvmResult> nomoSignEvmTransaction(JSAny args);
 
 extension type SignEvmArgs._(JSObject o) implements JSObject {
   external SignEvmArgs({String messageHex});
   external String messageHex;
+}
+
+extension type SignEvmResult._(JSObject o) implements JSObject {
+  external String sigHex;
 }
 
 class WalletBridge {
@@ -21,9 +25,15 @@ class WalletBridge {
   }
 
   static Future<String> signTransaction(String unsignedTxRaw) async {
-    final args = SignEvmArgs(messageHex: unsignedTxRaw);
-    final promise = nomoSignEvmTransaction(args);
-    final result = await promise.toDart;
-    return result.toDart;
+    try {
+      final args = SignEvmArgs(messageHex: unsignedTxRaw);
+      final promise = nomoSignEvmTransaction(args);
+      final result = await promise.toDart;
+      return result.sigHex;
+    } catch (e, s) {
+      print('Error signing transaction: $e');
+      print('Stacktrace: $s');
+      rethrow;
+    }
   }
 }
